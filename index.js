a451795a00a986c6aef903bcbd0cbf125b50195f
Coayer <copeyroyall@mac.com>
2025-11-17 13:49:39 +0000

    seo

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import { FlightRadar24API } from "./lib/flightradarapi";
 
+/*
+https://mapscaping.com/bounding-box-calculator/
+lat1 long1
+lat2 long2
+
+lat1 lat2 long1 long2
+*/
+
 const geoBounds = {
     global: null,
     icelandGreenland: "82.818307,45.386109,-66.594433,-7.848665",
     canadaAlaska: "83.761557,50.558120,-211.131045,-71.126161",
     northPacific: "48.968682,23.628425,-213.571543,-125.980548",
+    southPacific: "-77.281595,25.926439,-213.501958,-78.421869",
+    indianOcean: "-60.552416,0.070317,44.136283,111.676328"
 };
 
 export default {
